37577a3e1365e72c3233545b0ef78c9f04e10d01
Вадим <pvs.versia@gmail.com>
2025-02-14 15:38:40 +0200

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
-import React, {FC} from "react";
-import {Metadata} from "next";
+import React, { FC } from "react";
+import { Metadata } from "next";
+import { getServerSession } from "next-auth/next";
+import SimpleAlert from "@/components/All/Alerts/SimpleAlert.tsx";
+import Link from "next/link";
 
-import {getMetadata} from "./index.metadata";
+import { getMetadata } from "./index.metadata";
 
 const Home: FC = async () => {
-    return <div>Home</div>;
+    const session = await getServerSession();
+    if (session?.user) return <h1>Home</h1>;
+
+    return (
+            <SimpleAlert>
+                <div className="text-2xl">
+                    Hi Bud!! <br />
+                    You&apos;re still not authenticated.
+                    <br />
+                    Please login. <br />
+                    <Link className="text-blue-500 hover:text-blue-700 underline" href="/api/auth">
+                        Here is your link ...
+                    </Link>
+                    <br />
+                </div>
+            </SimpleAlert>
+    );
 };
 
 export const metadata: Metadata = await getMetadata({
@@ -13,3 +32,4 @@ export const metadata: Metadata = await getMetadata({
 });
 
 export default Home;
+
