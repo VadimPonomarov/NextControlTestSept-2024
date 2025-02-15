399adfd17f3ec479bfd5727a9bdae12adf2f3cd5
Вадим <pvs.versia@gmail.com>
2025-02-15 18:34:22 +0200

    UsersPage with hook

--- src/app/users/UsersClient.tsx
+++ src/app/users/UsersClient.tsx
@@ -4,6 +4,7 @@ import { IUser, IUsersResponse } from "@/common/interfaces/users.interfaces.ts";
 import { UserCard } from "@/components/Cards/UserCard/UserCard.tsx";
 import InfiniteScroll from "@/components/All/InfiniteScroll/InfiniteScroll.tsx";
 import { PaginationComponent } from "@/components/All/PaginationComponent/PaginationComponent.tsx";
+
 import { useUsersPagination } from "./useUsersPagination";
 
 interface IProps {
